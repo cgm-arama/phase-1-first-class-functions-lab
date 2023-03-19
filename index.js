bbc38675e3334bf5442c304538f75887dc674522
Eric <eric.arama@student.moringaschool.com>
2023-03-19 23:12:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+//*Code your solution in this file!*//
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(numberFour) {
+      return function (fare) {
+          return fare * numberFour;
+      };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+  };
+  const drivers = ["John", "Jane", "Bob", "Alice"];
